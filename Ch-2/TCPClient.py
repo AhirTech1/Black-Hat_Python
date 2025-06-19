08846a2af7052a6f9341813b125b932378a951c5
ghost <ahirtech.ap@gmail.com>
2025-06-19 23:17:56 +0530

    New Initial Commit + TCPClient

---
+++ Ch-2/TCPClient.py
@@ -0,0 +1,30 @@
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+#create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# socket.AF_INET means IPv4 will be used, socket.AF_INET6 for IPv6
+# socket.SOCK_STREAM for using TCP, socket.SOCK_DGRAM for UDP
+
+# connect the client
+client.connect((target_host, target_port))
+
+# send some data
+client.send(b"GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n")
+# GET - to retrieve some kind of resource
+# / - root path of the website (maybe home page)
+# HTTP/1.1 - version of HTTP
+#\r\n - return + newline
+
+# receive some data
+response = client.recv(4096)
+
+print(response)
+
+'''Assumptions in this code - 
+ - our connection will always succeed
+ - server is always expecting us to send data first
+ - server will always send us data back in a timely fashion
+'''
